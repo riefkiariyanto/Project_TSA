c9e6a2211d52811e2b3d22110aad16025939db42
riefkiariyanto <riefkiari199@gmail.com>
2022-09-16 12:23:02 +0700

    main

---
+++ upload_file_flutter/android/app/src/main/kotlin/com/example/upload_file_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.upload_file_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ upload_file_flutter/lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:chewie/chewie.dart';
+import 'package:video_player/video_player.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static const customSwatch = MaterialColor(
+    0xFFFF5252,
+    <int, Color>{
+      50: Color(0xFFFFEBEE),
+      100: Color(0xFFFFCDD2),
+      200: Color(0xFFEF9A9A),
+      300: Color(0xFFE57373),
+      400: Color(0xFFEF5350),
+      500: Color(0xFFFF5252),
+      600: Color(0xFFE53935),
+      700: Color(0xFFD32F2F),
+      800: Color(0xFFC62828),
+      900: Color(0xFFB71C1C),
+    },
+  );
+
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: customSwatch,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late VideoPlayerController videoPlayerController;
+  ChewieController? chewieController;
+
+  @override
+  void initState() {
+    super.initState();
+    _initPlayer();
+  }
+
+  void _initPlayer() async {
+    videoPlayerController = VideoPlayerController.network(' ');
+    await videoPlayerController.initialize();
+
+    chewieController = ChewieController(
+      videoPlayerController: videoPlayerController,
+      autoPlay: true,
+      looping: true,
+      additionalOptions: (context) {
+        return <OptionItem>[
+          OptionItem(
+            onTap: () => debugPrint('Option 1 pressed!'),
+            iconData: Icons.chat,
+            title: 'Option 1',
+          ),
+          OptionItem(
+            onTap: () => debugPrint('Option 2 pressed!'),
+            iconData: Icons.share,
+            title: 'Option 2',
+          ),
+        ];
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    videoPlayerController.dispose();
+    chewieController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Chewie Video Player"),
+      ),
+      body: chewieController != null
+          ? Padding(
+              padding: EdgeInsets.symmetric(vertical: 20),
+              child: Chewie(
+                controller: chewieController!,
+              ),
+            )
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
+    );
+  }
+}
