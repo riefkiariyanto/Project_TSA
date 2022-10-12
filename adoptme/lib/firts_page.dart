c638b2325e6b3bf892fe9fc00701099176f5aa32
riefkiariyanto <riefkiari199@gmail.com>
2022-10-12 22:28:06 +0700

    update dasboard

--- adoptme/lib/firts_page.dart
+++ adoptme/lib/firts_page.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class FirtsPage extends StatefulWidget {
   const FirtsPage({Key? key}) : super(key: key);
@@ -17,13 +14,49 @@ class _FirtsPageState extends State<FirtsPage> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Center(
-          child: Text(
-            'Hello !',
-            style: (TextStyle(
-              fontFamily: 'Gilroy',
-              fontSize: 52,
-            )),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Location',
+                      style: TextStyle(
+                          fontFamily: 'Gilroy',
+                          fontSize: 21,
+                          color: Colors.grey[700]),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Icon(CupertinoIcons.arrow_turn_right_down,
+                        color: Colors.blue[400], size: 25.0)
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Malang, Jawa Timur',
+                      style: TextStyle(fontFamily: 'Gilroy', fontSize: 23),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: GridView.count(crossAxisCount: 2, children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blue[400],
+                      ),
+                    ),
+                  ]),
+                ),
+              ],
+            ),
           ),
         ),
       ),
